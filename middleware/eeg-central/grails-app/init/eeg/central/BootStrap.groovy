5db70e4ce89393e26ffc87937becbc5cea174167
Martin Aigner <martin@maigner.net>
2023-11-03 19:40:51 +0100

    added spring security

--- middleware/eeg-central/grails-app/init/eeg/central/BootStrap.groovy
+++ middleware/eeg-central/grails-app/init/eeg/central/BootStrap.groovy
@@ -1,8 +1,37 @@
 package eeg.central
 
+import grails.gorm.transactions.Transactional
+
+@Transactional
 class BootStrap {
 
+    @Transactional
+    def setupUsers() {
+        final boolean flush = true
+        final boolean failOnError = true
+
+
+        def martin = User.findOrCreateWhere(username: "martin@maigner.net")
+        martin.password = "1234"
+        martin.save(flush: flush, failOnError: failOnError)
+
+        def adminRole = Role.findOrCreateWhere(authority: "ROLE_ADMIN")
+        adminRole.save(flush: flush, failOnError: failOnError)
+
+
+        def admins =  new RoleGroup(name: 'Admins')
+        admins.save(flush: flush, failOnError: failOnError)
+
+
+        RoleGroupRole.findOrCreateWhere(roleGroup: admins, role: adminRole).save(flush: flush, failOnError: failOnError)
+
+        UserRoleGroup.findOrCreateWhere(user: martin, roleGroup: admins).save(flush: flush, failOnError: failOnError)
+    }
+
     def init = { servletContext ->
+
+        setupUsers()
+
     }
     def destroy = {
     }
