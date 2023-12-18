4b2222dd13d9952df50e8000cda0fb0f6c1e2cbb
Martin Aigner <martin@maigner.net>
2023-12-18 21:01:07 +0100

    better navigation UX

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,75 +1,26 @@
 <script>
-  import "../app.css";
-  import { dev } from "$app/environment";
+	import "../app.css";
+	import { dev } from "$app/environment";
 
-  import { BottomNavItem, Heading, Hr } from "flowbite-svelte";
-  import {
-    HomeOutline,
-    MessagesOutline,
-    UserAddOutline,
-  } from "flowbite-svelte-icons";
-  import {
-    Navbar,
-    NavBrand,
-    NavLi,
-    NavUl,
-    NavHamburger,
-  } from "flowbite-svelte";
+	import { Hr } from "flowbite-svelte";
+	import BetaLogin from "$lib/dialog/BetaLogin.svelte";
+	import Navigation from "./Navigation.svelte";
 
-  import { page } from "$app/stores";
-  import BetaLogin from "$lib/dialog/BetaLogin.svelte";
-
-  let betaLoginOpen = !dev;
+	let betaLoginOpen = !dev;
 </script>
 
 <BetaLogin bind:open={betaLoginOpen} />
 
-<Navbar fluid={false} class="">
-  <NavBrand href="/">
-    <img src="header.png" class="mr-3 h-16 sm:h-20" alt="Logo" />
-  </NavBrand>
-  <NavHamburger />
-  <NavUl class="">
-    <div class="flex place-content-center">
-      {#if $page.route.id !== "/"}
-        <BottomNavItem btnName="Home" href="/" class="">
-          <HomeOutline
-            class="w-6 h-6 mb-1 {$page.route.id === '/'
-              ? 'text-green-950'
-              : 'text-green-600'}  group-hover:text-green-900 "
-          />
-        </BottomNavItem>
-      {/if}
-      {#if $page.route.id !== "/mitmachen"}
-        <BottomNavItem btnName="Mitmachen" href="/mitmachen">
-          <UserAddOutline
-            class="w-6 h-6 mb-1 {$page.route.id === '/mitmachen'
-              ? 'text-green-950'
-              : 'text-green-600'} group-hover:text-green-900 "
-          />
-        </BottomNavItem>
-      {/if}
-      {#if $page.route.id !== "/chatbot"}
-        <BottomNavItem btnName="Kontakt" href="/chatbot">
-          <MessagesOutline
-            class="w-6 h-6 mb-1 {$page.route.id === '/chatbot'
-              ? 'text-green-950'
-              : 'text-green-600'}  group-hover:text-green-900 "
-          />
-        </BottomNavItem>
-      {/if}
-    </div>
-  </NavUl>
-</Navbar>
+<Navigation />
 
 <slot />
 
 <div class="center mt-16">
-  <img src="sponsors.jpg" width="80%" alt="sponsors" />
+	<img src="sponsors.jpg" width="80%" alt="sponsors" />
 </div>
 
 <div class="center">
-  <a href="/impressum">Impressum</a>
+	<a href="/impressum">Impressum</a>
 </div>
 
 <Hr />
---
+++ website/src/routes/Navigation.svelte
@@ -0,0 +1,71 @@
+<script>
+    import { page } from "$app/stores";
+
+    import { BottomNavItem } from "flowbite-svelte";
+    import {
+        HomeOutline,
+        MessagesOutline,
+        UserAddOutline,
+    } from "flowbite-svelte-icons";
+
+    import {
+        Navbar,
+        NavBrand,
+        NavLi,
+        NavUl,
+        NavHamburger,
+    } from "flowbite-svelte";
+
+    
+    let hidden = true;
+
+    let closeNav = () => {
+        hidden = true;
+    };
+    
+
+</script>
+
+
+<Navbar let:NavContainer color="primary">
+    <NavContainer class="border px-4 py-1 rounded-lg">
+        <NavBrand href="/">
+            <img src="header.png" class="h-14 sm:h-18" alt="Logo" />
+        </NavBrand>
+        <NavHamburger onClick={ () => {
+            hidden = !hidden;
+        } }/>
+        <NavUl bind:hidden={hidden}>
+            <div class="flex place-content-center">
+                <NavLi href="/">
+                    <BottomNavItem btnName="Start" href="/" class="" on:click={closeNav}>
+                        <HomeOutline
+                            class="w-4 h-4 mb-1 {$page.route.id === '/'
+                                ? 'text-green-950'
+                                : 'text-green-600'}  group-hover:text-green-900 "
+                        />
+                    </BottomNavItem>
+                </NavLi>
+                <NavLi href="/mitmachen">
+                    <BottomNavItem btnName="Mitmachen" href="/mitmachen" on:click={closeNav}>
+                        <UserAddOutline
+                            class="w-4 h-4 mb-1 {$page.route.id === '/mitmachen'
+                                ? 'text-green-950'
+                                : 'text-green-600'} group-hover:text-green-900 "
+                        />
+                    </BottomNavItem>
+                </NavLi>
+                <NavLi href="/chatbot">
+                    <BottomNavItem btnName="Kontakt" href="/chatbot" on:click={closeNav}>
+                        <MessagesOutline
+                            class="w-4 h-4 mb-1 {$page.route.id === '/chatbot'
+                                ? 'text-green-950'
+                                : 'text-green-600'}  group-hover:text-green-900 "
+                        />
+                    </BottomNavItem>
+                </NavLi>
+            </div>
+        </NavUl>
+    </NavContainer>
+</Navbar>
+
