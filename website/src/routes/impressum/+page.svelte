27fcda6b627b1465c0604f078d9f0e71f50b7c08
Martin Aigner <martin@maigner.net>
2024-01-23 20:10:14 +0100

    imprint

--- website/src/routes/impressum/+page.svelte
+++ website/src/routes/impressum/+page.svelte
@@ -1,84 +1,126 @@
 <script>
     import { Card, Heading } from "flowbite-svelte";
+    import { AccordionItem, Accordion } from "flowbite-svelte";
 </script>
 
 <div class="text-center">
-    <Heading tag="h2">Impressum</Heading>
+    <Heading tag="h3" class="text-primary-800 mt-6">Impressum</Heading>
 </div>
 
 <div class="flex place-content-center">
     <Card class="m-2 max-w-3xl" size="xl">
-        <section>
-            <h2>Angaben gemäß § 5 TMG:</h2>
-            <p>Name der Energiegemeinschaft</p>
-            <p>Musterstraße 123</p>
-            <p>12345 Musterstadt</p>
-            <p>Telefon: 01234 567890</p>
-            <p>E-Mail: info@energiegemeinschaft.de</p>
-        </section>
-        <section>
-            <h2>Vertreten durch:</h2>
-            <p>Vorname Nachname</p>
-        </section>
-        <section>
-            <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h2>
-            <p>Vorname Nachname</p>
-        </section>
-        <section>
-            <h2>Registereintrag:</h2>
-            <p>Eintragung im Vereinsregister.</p>
-            <p>Registergericht: Musterstadt</p>
-            <p>Registernummer: VR12345</p>
-        </section>
-        <section>
-            <h2>Umsatzsteuer-ID:</h2>
-            <p>
-                Umsatzsteuer-Identifikationsnummer gemäß §27 a
-                Umsatzsteuergesetz:
-            </p>
-            <p>DE123456789</p>
-        </section>
-        <section>
-            <h2>Haftungsausschluss:</h2>
-            <p>Haftung für Inhalte</p>
-            <p>Haftung für Links</p>
-        </section>
-        <section>
-            <h2>Datenschutzerklärung:</h2>
-            <p>Hier finden Sie unsere Datenschutzerklärung.</p>
-        </section>
-        <footer>
-            <p>
-                Quelle: <a href="https://www.e-recht24.de" target="_blank"
-                    >e-recht24.de</a
-                >
-            </p>
-        </footer>
-        
-    </Card>
-</div>
+        <Accordion class="w-80">
+            <AccordionItem open>
+                <span slot="header">Verantwortlich für den Inhalt</span>
 
-<div>
-    Bildquellen:
-    <div>
-        <ul>
-            <li>
-                https://commons.wikimedia.org/wiki/Category:Smart_meters#/media/File:Slimme_meters_van_Enexis,_Oude_Pekela_(2018)_01.jpg
-            </li>
-            <li>
-                https://commons.wikimedia.org/wiki/File:004_2017_05_20_Erneuerbare_Energie.jpg
-                © Friedrich Haag&nbsp;/&nbsp;<a href="/wiki/Main_Page" title="Main Page">Wikimedia Commons</a>
-            </li>
-            <li>
-                <a href="https://commons.wikimedia.org/wiki/File:Herten_Westerholt_-_Freiheit_03_ies.jpg">Frank Vincentz</a>, <a href="https://creativecommons.org/licenses/by-sa/3.0">CC BY-SA 3.0</a>, via Wikimedia Commons
-            </li>
-            <li>
-                <a href="https://commons.wikimedia.org/wiki/File:%22Miteinander-Weg%22_in_Gilching.png">XoMEoX</a>, <a href="https://creativecommons.org/licenses/by/4.0">CC BY 4.0</a>, via Wikimedia Commons
-            </li>
-            <li>
-                <a href="https://commons.wikimedia.org/wiki/File:Elbe_Parey-Polte_03.jpg">Georgfotoart</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons
-            </li>
-        </ul>
-        
-    </div>
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Erneuerbare-Energie-Gemeinschaft ISCHLSTROM
+                    <br />
+                    ZVR: 1915125185
+                    <br />
+                    Obfrau DI Katharina Thomson 
+                    <br />Faschlweg 39 
+                    <br />
+                    4820 Bad Ischl
+                    <br />
+                    E-Mail: info@ischlstrom.org
+                </p>
+
+                <p class="text-gray-500 dark:text-gray-400">
+                    
+                </p>
+            </AccordionItem>
+            <AccordionItem>
+                <span slot="header">Datenschutz</span>
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Wir sammeln keine unnötigen Daten.
+                </p>
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Wir geben keine Daten weiter.
+                </p>
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Wir achten Ihre Privatspäre.
+                </p>
+                
+            </AccordionItem>
+
+            <AccordionItem>
+                <span slot="header">Technische Anprechpartnerin</span>
+
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Erneuerbare-Energie-Gemeinschaft ISCHLSTROM
+                    <br />
+                    Dr. Martin Aigner 
+                    <br />Loskoglweg 14
+                    <br />
+                    4820 Bad Ischl
+                    <br />
+                    E-Mail: martin.aigner@ischlstrom.org
+                </p>
+
+                <p class="text-gray-500 dark:text-gray-400">
+                    
+                </p>
+            </AccordionItem>
+
+            <AccordionItem>
+                <span slot="header">Bildquellen</span>
+
+                <div class="text-xs text-gray-500 dark:text-gray-400">
+                    <ul>
+                        <li>
+                            <a
+                                href="https://commons.wikimedia.org/wiki/File:Slimme_meters_van_Enexis,_Oude_Pekela_(2018)_01.jpg"
+                                >Donald Trung Quoc Don (Chữ Hán: 徵國單) - Wikimedia
+                                Commons.</a
+                            >,<br />
+                            <a href="https://creativecommons.org/licenses/by-sa/4.0"
+                                >CC BY-SA 4.0</a
+                            >, via Wikimedia Commons
+                        </li>
+                        <li>
+                            <a
+                                href="https://commons.wikimedia.org/wiki/File:004_2017_05_20_Erneuerbare_Energie.jpg"
+                            >
+                                © Friedrich Haag&nbsp;/&nbsp;</a
+                            ><a href="/wiki/Main_Page" title="Main Page"
+                                >Wikimedia Commons</a
+                            >
+                        </li>
+                        <li>
+                            <a
+                                href="https://commons.wikimedia.org/wiki/File:Herten_Westerholt_-_Freiheit_03_ies.jpg"
+                            >
+                                Frank Vincentz
+                            </a>,
+                            <a href="https://creativecommons.org/licenses/by-sa/3.0"
+                                >CC BY-SA 3.0</a
+                            >, via Wikimedia Commons
+                        </li>
+                        <li>
+                            <a
+                                href="https://commons.wikimedia.org/wiki/File:%22Miteinander-Weg%22_in_Gilching.png"
+                                >XoMEoX</a
+                            >,
+                            <a href="https://creativecommons.org/licenses/by/4.0"
+                                >CC BY 4.0</a
+                            >, via Wikimedia Commons
+                        </li>
+                        <li>
+                            <a
+                                href="https://commons.wikimedia.org/wiki/File:Elbe_Parey-Polte_03.jpg"
+                                >Georgfotoart</a
+                            >,
+                            <a href="https://creativecommons.org/licenses/by-sa/4.0"
+                                >CC BY-SA 4.0</a
+                            >, via Wikimedia Commons
+                        </li>
+                    </ul>
+                </div>
+                
+            </AccordionItem>
+        </Accordion>
+
+       
+    </Card>
 </div>
