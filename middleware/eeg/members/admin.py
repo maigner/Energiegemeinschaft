e211d2ef24ae315971cb9b5156d20e17ed372ed3
Martin Aigner <martin@maigner.net>
2024-04-26 22:17:13 +0200

    data model for new approval tasks

--- middleware/eeg/members/admin.py
+++ middleware/eeg/members/admin.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 
 from .models import Member
 from .models import BoardApproval
+from .models import MemberApprovalTask
 
 admin.site.register(Member)
 admin.site.register(BoardApproval)
+admin.site.register(MemberApprovalTask)
--- middleware/eeg/members/models.py
+++ middleware/eeg/members/models.py
@@ -15,9 +15,19 @@ class BoardApproval(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     date_time = models.DateTimeField(auto_now_add=True)
     new_member_approved = models.CharField(max_length=200)
+    new_member_email = models.EmailField(null=True)
     answer = models.CharField(max_length=200)
     def __str__(self):
         return f"{self.member} @ {self.date_time}: {self.new_member_approved}: {self.answer}"
+
+
+class MemberApprovalTask(models.Model):
+    date_time = models.DateTimeField()
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    def __str__(self):
+        return f"{self.email}: {self.name}, {self.address}"
 
 
 class MeasurementPoint(models.Model):
