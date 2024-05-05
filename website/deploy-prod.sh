12f2a0726e9c04dab646be0d6da92be2415a15b4
Martin Aigner <martin@maigner.net>
2024-05-05 22:42:36 +0200

    fixed deploy script

--- website/deploy-prod.sh
+++ website/deploy-prod.sh
@@ -11,6 +11,8 @@ zip -q ischlstrom-website-package.zip .ebextensions
 zip -q ischlstrom-website-package.zip global-bundle.pem
 zip -q ischlstrom-website-package.zip package.json
 zip -q ischlstrom-website-package.zip package-lock.json
+zip -q ischlstrom-website-package.zip member_locations.json
+
 
 # get the latest git hash and deploy
 eb deploy --profile ischlstrom-admin --staged -l `git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/\1/"`
