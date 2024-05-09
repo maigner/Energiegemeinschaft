1ece2d5d3a1a524d9484ef0b667d60d59a0bbd3e
Martin Aigner <martin@maigner.net>
2024-05-09 21:01:59 +0200

    cleaning up

--- website/src/routes/board/map/+page.server.js
+++ website/src/routes/board/map/+page.server.js
@@ -1,19 +1,11 @@
 import { MAPBOX_TOKEN } from "$env/static/private";
 import { getMemberLocations } from "$lib/server/db/members/member";
-import { readFileSync } from 'node:fs';
 
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch, params, parent, locals }) {
 
-    const jsonFile = readFileSync('member_locations.json').toString();
-
-    //const memberLocations = JSON.parse(jsonFile);
-
     const memberLocations = await getMemberLocations();
-
-
-
 
     return {
         mapboxToken: MAPBOX_TOKEN,
