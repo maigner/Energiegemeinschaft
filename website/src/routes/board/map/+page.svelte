a6c249195986c06afa264d9b7145a18a682c41e3
Martin Aigner <martin@maigner.net>
2024-05-15 22:06:23 +0200

    cleaning up

--- website/src/routes/board/map/+page.svelte
+++ website/src/routes/board/map/+page.svelte
@@ -30,20 +30,6 @@
             zoom: initialState.zoom,
         });
 
-        /*
-        map.on("load", () => {
-            map.addLayer({
-                id: "terrain-data",
-                type: "line",
-                source: {
-                    type: "vector",
-                    url: "mapbox://mapbox.mapbox-terrain-v2",
-                },
-                "source-layer": "contour",
-            });
-        });
-        */
-
         console.log(data.memberLocations);
         
         data.memberLocations.forEach(location => {
