e4669b65faf1482d481a73b912f88d0191a5c143
Martin Aigner <martin@maigner.net>
2024-06-16 23:11:33 +0200

    faq

--- website/src/routes/faq/+page.svelte
+++ website/src/routes/faq/+page.svelte
@@ -31,9 +31,17 @@
         {
             question: "Wie hoch ist der Mitgliedsbeitrag?",
             answer: "20 EURO pro Jahr, bzw. 5 EURO pro Quartal"
+        },
+        {
+            question: "Werden außer dem Mitgliedsbeitrag eine Grundeinlage und eine Beitrittsgebühr verrechnet?",
+            answer: "Aktuell wird nur ein Mitgliedsbeitrag in der Höhe von € 20,- jährlich, der pro Quartal mit € 5,- verrechnet wird, verrechnet. Zusätzliche Gebühren werden vom Vorstand derzeit nicht vorgeschlagen und sind wie alle Tarifänderungen von der Generalversammlung zu beschließen, d.h. durch die Mitglieder zu beschließen."
         }
     ]
     const faqCommon = [
+        {
+            question: "Muss bei einem Beitritt zu Ischlstrom der derzeitige Energielieferant bzw. - abnehmer gekündigt werden?",
+            answer: "Keinesfalls. Sie benötigen weiter hin einen Lieferanten bzw. Abnehmer, der Sie mit Energie auch dann versorgt bzw. Ihnen abnimmt, wenn durch die Mitglieder von Ischlstrom zu wenig Sonnenstrom produziert wird, bzw. zu wenig Bedarf an Sonnenstrom durch die Ischlstrom – Mitglieder besteht."
+        },
         {
             question: "Wie ist der Verein strukturiert?",
             answer: "Alle Details finden sich in den Statuten.",
