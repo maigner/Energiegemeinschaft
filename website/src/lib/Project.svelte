204eaafcca0a76f6a9fe2c4afcdec41b6c1ded50
Martin Aigner <martin@maigner.net>
2024-06-17 23:30:18 +0200

    Project Component Margin

--- website/src/lib/Project.svelte
+++ website/src/lib/Project.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="min-w-fit">
-    <Card {img} class="m-2 max-w-3xl" size="xl">
+    <Card {img} class="m-2 max-w-3xl mb-10" size="xl">
         <h5
             class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
         >
