7d4cc9f4cf4ada85fc6f245ec82e934376edac81
Martin Aigner <martin@maigner.net>
2024-10-24 12:08:59 +0200

    handling registration links

---
+++ website/src/routes/(website)/gv/[token]/[year]/[email]/+layout.server.js
@@ -0,0 +1,38 @@
+import { NEWSLETTER_TOKEN_GV_2024 } from '$env/static/private';
+import { getUsersByEmail } from '$lib/server/db/members/member';
+
+function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+}
+
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load({ parent, locals, params }) {
+
+    // member info
+    let { session } = await parent();
+
+    // @ts-ignore
+    const users = await getUsersByEmail(session?.user?.email);
+
+    console.log({ params });
+
+    if (params.token !== NEWSLETTER_TOKEN_GV_2024) {
+        return {
+            error: "invalid token"
+        }
+    }
+
+    if (!validateEmail(params.email)) {
+        return {
+            error: "invalid email"
+        }
+    }
+
+
+    return {
+        users: users,
+        newsletterEmail: params.email
+    }
+
+}
---
+++ website/src/routes/(website)/gv/[token]/[year]/[email]/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { Heading } from "flowbite-svelte";
+    export let data;
+</script>
+
+
+{#if data.error} 
+    {data.error}
+{:else}
+    
+    <div class="text-center">
+        <Heading tag="h1" class="text-primary-600 mt-8">Danke!</Heading>
+
+        <p class="mt-8">
+            Für Ihre Anmeldung zur Generalversammlung 2024
+        </p>
+
+        <p class="mt-4">
+            {data.newsletterEmail}
+        </p>
+
+
+
+        <p class="mt-8">
+            Sollte Ihre Anmeldung ein Versehen gewesen sein, dann lassen Sie uns das bitte formlos via info@ischlstrom.org wissen.
+        </p>
+
+
+        <p class="mt-16">
+            Ihr Vorstand von ISCHLSTROM
+        </p>
+        
+        
+    </div>
+{/if}
