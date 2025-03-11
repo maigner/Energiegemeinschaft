5427ef47539d3ca1d13e5233311838146cdc20da
Martin Aigner <martin@maigner.net>
2025-03-11 16:01:19 +0100

    info flyer to front page

--- website/src/routes/(website)/+page.svelte
+++ website/src/routes/(website)/+page.svelte
@@ -4,6 +4,7 @@
     import { Blockquote } from "flowbite-svelte";
     import Savings from "./Savings.svelte";
     import Fab from "$lib/Fab.svelte";
+    import { QuoteSolid } from "flowbite-svelte-icons";
     //import { JSONEditor } from "svelte-jsoneditor";
 
 
@@ -40,8 +41,49 @@
     </figure>
 </div>
 
-
+<!--
 <Savings bind:data />
+-->
+
+
+<div class="flex place-content-center">
+    <Project img="/250320_Flyer Infoveranstaltung.jpg" showMore={false}>
+        <div slot="content">
+
+            <figure class="mx-auto text-center">
+                <QuoteSolid
+                    class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600"
+                />
+                <Blockquote alignment="center" size="2xl">
+
+                    <p class="mt-2">"Wir freuen uns auf Ihr Kommen!"</p>
+                </Blockquote>
+                <figcaption
+                    class="flex justify-center items-center mt-6 space-x-3 rtl:space-x-reverse"
+                >
+                    <img
+                        class="w-6 rounded-full"
+                        src="/people/katharina_thomson.webp"
+                        alt="Katharina Thomson"
+                    />
+                    <div
+                        class="flex items-center divide-x-2 rtl:divide-x-reverse divide-gray-500 dark:divide-gray-700"
+                    >
+                        <cite
+                            class="pe-3 font-medium text-gray-900 dark:text-white"
+                            >Katharina Thomson</cite
+                        >
+                        <cite
+                            class="ps-3 text-sm font-light text-gray-500 dark:text-gray-400"
+                            >Obfrau von ISCHL STROM</cite
+                        >
+                    </div>
+                </figcaption>
+            </figure>
+
+        </div>
+    </Project>
+</div>
 
 <div class="text-center">
     <Heading tag="h3" class="text-primary-600 mt-6"
