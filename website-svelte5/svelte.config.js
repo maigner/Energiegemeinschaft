c685528d28e5f147f19304a3d15ce76a1592b9d9
Martin Aigner <martin@maigner.net>
2025-05-02 14:57:00 +0200

    cleaning up

--- website-svelte5/svelte.config.js
+++ website-svelte5/svelte.config.js
@@ -1,19 +1,14 @@
-//import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-node';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-		// for contact form. TODO: fix
-		csrf: {
-			checkOrigin: false,
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    // for contact form. TODO: fix
+    csrf: {
+      checkOrigin: false,
+    }
+  },
 };
 
 export default config;
