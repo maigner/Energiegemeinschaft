401dbd33e323ca3bfec64c83457479fdc76855bf
Martin Aigner <martin@maigner.net>
2025-07-06 17:12:01 +0200

    open meteo data model

--- middleware/eeg/weather/models.py
+++ middleware/eeg/weather/models.py
@@ -15,3 +15,24 @@ class HistoricWeatherData(models.Model):
 
     def __str__(self):
         return f"Weather at {self.timestamp.strftime('%Y-%m-%d %H:%M')}"
+    
+    
+    
+class WeatherData(models.Model):
+    time = models.DateTimeField(primary_key=True)
+    temperature_2m = models.FloatField(help_text="°C")
+    cloud_cover = models.FloatField(help_text="%")
+    rain = models.FloatField(help_text="mm")
+    snowfall = models.FloatField(help_text="cm")
+    snow_depth = models.FloatField(help_text="m")
+
+    cloud_cover_low = models.FloatField(help_text="%")
+    cloud_cover_mid = models.FloatField(help_text="%")
+    cloud_cover_high = models.FloatField(help_text="%")
+
+    visibility = models.FloatField(help_text="m")
+    relative_humidity_2m = models.FloatField(help_text="%")
+    dew_point_2m = models.FloatField(help_text="°C")
+
+    def __str__(self):
+        return f"Weather at {self.time}"
