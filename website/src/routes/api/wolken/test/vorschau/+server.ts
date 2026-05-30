1af36285bdae111c5c740866c0a3e859a5da9e2a
Martin Aigner <martin@maigner.net>
2026-05-30 12:48:06 +0200

    better logging

--- website/src/routes/api/wolken/test/vorschau/+server.ts
+++ website/src/routes/api/wolken/test/vorschau/+server.ts
@@ -5,7 +5,8 @@ import { json } from '@sveltejs/kit';
 /** @type {import('../../$types').RequestHandler} */
 export async function GET(event) {
 
-    console.log({event});
+    console.log("public weather api called");
+    
 
 
     const forcast = await getCloudForecast();
